7ab631fec62c3940a1ff66a778007219e08044ff
Yunhao <yz2327@cornell.edu>
2023-02-23 14:15:58 -0500

    try openssh

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use openssh::*;
+
+#[tokio::main]
+async fn main() {
+    let session = Session::connect("ssh://Yunhao@server0", KnownHosts::Strict)
+        .await
+        .unwrap();
+
+    let ls = session.command("ls").output().await.unwrap();
+    println!("{}", String::from_utf8(ls.stdout).unwrap());
+
+    let whoami = session.command("whoami").output().await.unwrap();
+    println!("{}", String::from_utf8(whoami.stdout).unwrap());
+
+    session.close().await.unwrap();
 }
