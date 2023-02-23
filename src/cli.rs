c793ff2140058e92906a27fc1dd6db7b8cf209ca
Yunhao <yz2327@cornell.edu>
2023-02-23 17:02:37 -0500

    Add cli support

---
+++ src/cli.rs
@@ -0,0 +1,27 @@
+use {
+    clap::{
+        Parser,
+        Subcommand,
+    },
+};
+
+#[derive(Parser, Debug)]
+#[clap(about = "A cli for the chance oracle benchmark")]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub action:     Action,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Action {
+    #[clap(about = "Evaluate a target system")]
+    Eval {
+        #[clap(
+            short = 's', long = "sys",
+            default_value = "testenv",
+            help = "Specify the target system"
+        )]
+        target: String,
+    },
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+pub mod cli;
 pub mod error;
 pub mod config;
 
 use openssh::*;
+use clap::Parser;
+use cli::{Cli, Action};
 use crate::error::OracleError;
-
 
 #[tokio::main]
 async fn main() -> Result<(), OracleError> {
+    // Parse arguments
+    let cli = Cli::parse();
+    match cli.action {
+        Action::Eval { target } => {
+            println!("target is {}", target);
+        }
+    }
+
+    // Config network latency
     config::config_latency()?;
 
     // Example of openssh
@@ -20,6 +31,7 @@ async fn main() -> Result<(), OracleError> {
     session.close()
         .await
         .map_err(|_| OracleError::SshCloseFailed)?;
+
 
     Ok(())
 }
