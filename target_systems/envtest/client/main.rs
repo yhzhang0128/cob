bc93f9ada70694fbcc80abf85d6997b550151a00
Yunhao Zhang <yz2327@cornell.edu>
2023-02-27 18:54:54 -0500

    Debug

--- target_systems/envtest/client/main.rs
+++ target_systems/envtest/client/main.rs
@@ -1,9 +1,9 @@
-use std::fs::File;
-use std::io::prelude::*;
+// use std::fs::File;
+// use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::create("/users/Yunhao/client_log.txt")?;
-    file.write_all(b"This is envtest client.\n")?;
+    // let mut file = File::create("/users/Yunhao/client_log.txt")?;
+    // file.write_all(b"This is envtest client.\n")?;
 
     println!("This is envtest client.");
     loop {};
--- target_systems/envtest/server/main.rs
+++ target_systems/envtest/server/main.rs
@@ -1,9 +1,9 @@
-use std::fs::File;
-use std::io::prelude::*;
+// use std::fs::File;
+// use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::create("/users/Yunhao/server_log.txt")?;
-    file.write_all(b"This is envtest server.")?;
+    // let mut file = File::create("/users/Yunhao/server_log.txt")?;
+    // file.write_all(b"This is envtest server.")?;
 
     println!("This is envtest server.");
     loop {};
