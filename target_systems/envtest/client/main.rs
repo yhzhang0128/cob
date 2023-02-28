b49b6a9f8d5f906addd2f4c758f4c6a5bdb7e251
Yunhao Zhang <yz2327@cornell.edu>
2023-02-27 20:26:11 -0500

    Parse args in envtest

--- target_systems/envtest/client/main.rs
+++ target_systems/envtest/client/main.rs
@@ -1,10 +1,26 @@
+use clap::Parser;
 // use std::fs::File;
 // use std::io::prelude::*;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   #[arg(short, long)]
+   config: String,
+
+   #[arg(short, long)]
+   log: String,
+
+   #[arg(short, long)]
+   idx: u8,
+}
+
 
 fn main() -> std::io::Result<()> {
     // let mut file = File::create("/users/Yunhao/client_log.txt")?;
     // file.write_all(b"This is envtest client.\n")?;
+    let args = Args::parse();
 
-    println!("This is envtest client.");
+    println!("This is envtest client#{}.", args.idx);
     loop {};
 }
