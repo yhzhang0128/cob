86696a85673b87cdfd45edce00a61aa3030c1b55
Yunhao Zhang <yz2327@cornell.edu>
2023-03-27 21:32:59 -0400

    debug

--- src/spawn.rs
+++ src/spawn.rs
@@ -307,12 +307,13 @@ pub async fn spawn_envtest_geo<'a>(ssh_conns: &'a HashMap<String, Session>,
                 // add network delay
                 // sudo tc qdisc add dev enp1s0d1 root netem delay ??ms
                 println!("[DEBUG] client {} latency {}ms", client, latency);
-                s.command("sudo")
-                    .args(["tc", "qdisc", "add", "dev", "enp1s0d1", "root", "netem", "delay"])
-                    .arg(format!("{}ms", latency))
-                    .output()
-                    .await
-                    .map_err(|_| OracleError::SshCommandFailed)?;
+                process.push(s.command("sudo")
+                             .args(["tc", "qdisc", "add", "dev", "enp1s0d1", "root", "netem", "delay"])
+                             .arg(format!("{}ms", latency))
+                             .spawn()
+                             .await
+                             .map_err(|_| OracleError::SshCommandFailed)?
+                );
             }
         }
         client_id += 1;
