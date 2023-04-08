003e9820597c526e630bbee6d0851785f7535a76
Yunhao <yz2327@cornell.edu>
2023-04-08 15:38:34 -0400

    debug

--- src/kill.rs
+++ src/kill.rs
@@ -8,55 +8,56 @@ pub async fn killall(target: &TargetType, print: bool) -> Result<(), OracleError
     let config = read_config(target)?;
     let ssh_conns = start_ssh_conns(&config["hostnames"]).await?;
 
-    // Kill all processes
-    for bin in &config["binary-files"] {
-        //println!("Killing {}", bin);
+    let client_bin = &config["binary-files"][0];
+    let server_bin = &config["binary-files"][1];
+
+    // Kill client processes
+    for (host, s) in &ssh_conns {
+        let kill1 = s.command("killall")
+            .args([client_bin.as_str()])
+            .output()
+            .await
+            .map_err(|_| OracleError::SshCommandFailed)?;
+        if print {
+            println!("Kill process {} on host {}.", client_bin, host);
+            if kill1.stderr.len() > 0 {
+                println!("  [warning] stderr from {}: {:?}", host, String::from_utf8(kill1.stderr).unwrap());
+            }
+        }
+    }
+    
+    // Kill speedbump processes
+    if config["binary-files"].len() > 2 {
+        let speedbump_bin = &config["binary-files"][2];
         for (host, s) in &ssh_conns {
-            let kill = s.command("killall")
-                .args([bin.as_str()])
+            let kill1 = s.command("killall")
+                .args([speedbump_bin.as_str()])
                 .output()
                 .await
                 .map_err(|_| OracleError::SshCommandFailed)?;
             if print {
-                println!("Kill process {} on host {}.", bin, host);
-                if kill.stderr.len() > 0 {
-                    println!("  [warning] stderr from {}: {:?}", host, String::from_utf8(kill.stderr).unwrap());
+                println!("Kill process {} on host {}.", speedbump_bin, host);
+                if kill1.stderr.len() > 0 {
+                    println!("  [warning] stderr from {}: {:?}", host, String::from_utf8(kill1.stderr).unwrap());
                 }
             }
         }
     }
-
-    // let client_bin = &config["binary-files"][0];
-    // let server_bin = &config["binary-files"][1];
-    // Kill client processes
-    // for (host, s) in &ssh_conns {
-    //     let kill1 = s.command("killall")
-    //         .args([client_bin.as_str()])
-    //         .output()
-    //         .await
-    //         .map_err(|_| OracleError::SshCommandFailed)?;
-    //     if print {
-    //         println!("Kill process {} on host {}.", client_bin, host);
-    //         if kill1.stderr.len() > 0 {
-    //             println!("  [warning] stderr from {}: {:?}", host, String::from_utf8(kill1.stderr).unwrap());
-    //         }
-    //     }
-    // }
-
+    
     // Kill server processes
-    // for (host, s) in &ssh_conns {
-    //     let kill2 = s.command("killall")
-    //         .args([server_bin.as_str()])
-    //         .output()
-    //         .await
-    //         .map_err(|_| OracleError::SshCommandFailed)?;
-    //     if print {
-    //         println!("Kill process {} on host {}.", server_bin, host);
-    //         if kill2.stderr.len() > 0 {
-    //             println!("  [warning] stderr from {}: {:?}", host, String::from_utf8(kill2.stderr).unwrap());
-    //         }
-    //     }
-    // }
+    for (host, s) in &ssh_conns {
+        let kill3 = s.command("killall")
+            .args([server_bin.as_str()])
+            .output()
+            .await
+            .map_err(|_| OracleError::SshCommandFailed)?;
+        if print {
+            println!("Kill process {} on host {}.", server_bin, host);
+            if kill3.stderr.len() > 0 {
+                println!("  [warning] stderr from {}: {:?}", host, String::from_utf8(kill3.stderr).unwrap());
+            }
+        }
+    }
 
     Ok(())
 }
